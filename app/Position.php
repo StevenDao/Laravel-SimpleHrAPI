48ca7ba9df101720fc1b62c49a1323989a27a27d
Steven Dao <nataku235@gmail.com>
2018-11-21 17:26:20 -0500

    Add Position Model

---
+++ app/Position.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Position
+ * @package App
+ * @property string title
+ * @property mixed created_at
+ * @property mixed updated_at
+ */
+class Position extends Model
+{
+    public $fillable = [
+        'title',
+    ];
+}

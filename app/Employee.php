811e060f95446f9376716cc4f69fe4dfaf6069d1
Steven Dao <nataku235@gmail.com>
2018-11-21 16:23:03 -0500

    Add Employee Model

---
+++ app/Employee.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Employee
+ * @package App
+ * @property string first_name
+ * @property string last_name
+ * @property int salary
+ * @property boolean is_active
+ * @property mixed hired_date
+ * @property mixed updated_at
+ * @property mixed created_at
+ */
+class Employee extends Model
+{
+    public $fillable = [
+        'first_name',
+        'last_name',
+        'salary',
+        'is_active',
+        'hired_date',
+    ];
+}
